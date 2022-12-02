01c2d0f736ed69516926e103a5578f92c066fcbd
Binou05 <sabine.arnaud-bertoneri@sncf.fr>
2022-12-02 17:23:07 +0100

    cards css finish
    Co-authored-by: Fred <frederic@galline.fr>

--- museum/src/components/Gallery/GalleryButton.js
+++ museum/src/components/Gallery/GalleryButton.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 
 
 
-const GalleryButton = (props) => {
-    return (
-        <div>
-            <button className="button" type={props.type || "button"}
-            onClick={props.onClick}>
- 
-            {props.children}
-            Ajoutez +</button>
-        </div> 
-    );
-};
+const GalleryButton = () => {
+    const [data, setData]= useState([])
+    const[offset, setOffset]= useState(0)
+    useEffect(() => {
+        const fetchData = async()=> {
+            try {
+                const res = await fetch("")
+                const data = await res.json()
+                setData(pre =>[...pre,...data])
+            } catch (error){
+                console.log(error)
+            }
+        }
+        fetchData()
+        },[offset])
+
+        useEffect(()=>{
+            const handleScroll=(e)=> {
+                const scrollHeight= e.target.documentElement.scrollHeight
+                const currentHeight = e.target.documentElement.srollTop + window.innerHeight
+                if (currentHeight + 1>= scrollHeight){
+                    setOffset(offset + 5)
+                }
+            }
+            window.addEventListener("scroll", handleScroll)
+        },[offset])
+            return( 
+                <div className="gallerybutton">
+                    <div className="product-list">
+                        {data && data.map(product =>(
+                            <div className ='product-item' key={product.id}>
+                                <img src={product.images[0]}alt='pictures'/>
+                                <div className='product-info'>
+                                   <h4 className='product-name'>{product.name}</h4>
+                                   <p className='product-title'>{product.title}</p>
+                    </div>
+                  </div>
+                    ))}
+                    </div>
+                    </div>
+            )
+        }
+                
+           
+
 
 export default GalleryButton;
