ddb370f893f6960f68c1f4fd21a22dc7d3851c58
FannyBonnet <fanny.bonnet@sncf.fr>
2022-12-02 15:19:42 +0100

    filters updated with MUI

--- museum/src/components/PrimaryContainer/NavBar/filter/DatingPeriod.js
+++ museum/src/components/PrimaryContainer/NavBar/filter/DatingPeriod.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import test from './testList';
 
 function DatingPeriod() {
-    return(
-        <div>
-            Période
-        </div>
-    );
+    return (
+        <Autocomplete
+          disablePortal
+          id="artist"
+          options={test}
+          sx={{ width: 100 }}
+          renderInput={(params) => <TextField {...params} label="Période" />}
+        />
+      );
 }
 
 export default  DatingPeriod;
