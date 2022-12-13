a04f83106b2fdf7c056f8f9288720ff4bacdb28a
fredericgalline <frederic@galline.fr>
2022-12-13 15:49:40 +0100

    fix 10k bug

--- museum/src/components/PrimaryContainer/Content/FeatureImage/FeatureImage.js
+++ museum/src/components/PrimaryContainer/Content/FeatureImage/FeatureImage.js
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import * as React from "react";
 
 const FeatureImage = ({ image }) => {
-  return <img src={image} alt="" style={{ animation: `fadeIn 1s` }}></img>;
+  return (
+    <div>
+      <img src={image} alt="" style={{ animation: `fadeIn 1s` }}></img>
+    </div>
+  );
 };
 
 export default FeatureImage;
