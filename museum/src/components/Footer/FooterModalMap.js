9dd834744d95b8d7ea6c426cb3d36ee9d0923b61
HoiAn2010 <Alinesonza@aol.com>
2022-12-14 09:29:20 +0100

    Merge branch 'dev' of github.com:WildCodeSchool/2022-09-JS-Marseille-SNCF-P2-Museum into footer

--- museum/src/components/Footer/FooterModalMap.js
+++ museum/src/components/Footer/FooterModalMap.js
@@ -16,7 +16,7 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 800,
   height: 600,
-  borderRadius: 5,
+  borderRadius: 1,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
