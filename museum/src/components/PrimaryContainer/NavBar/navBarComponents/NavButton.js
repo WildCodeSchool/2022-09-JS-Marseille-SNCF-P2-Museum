9f5eca99a1bf4234c498b41ecedd8c7190f823b0
fredericgalline <frederic@galline.fr>
2022-12-20 17:20:33 +0100

    fix boutton de FANNNNNNY

--- museum/src/components/PrimaryContainer/NavBar/navBarComponents/NavButton.js
+++ museum/src/components/PrimaryContainer/NavBar/navBarComponents/NavButton.js
@@ -6,6 +6,8 @@ import List from '@mui/material/List';
 import ListItemText from '@mui/material/ListItemText';
 import IsSelectedContext from '../../../../contexts/IsSelectedContext'
 import SelectedCardBody from "./SelectedCards/SelectedCardBody"
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Typography from "@mui/material/Typography";
 
 function NavButton() {
     const {isSelected, setIsSelected} = useContext(IsSelectedContext);    
@@ -20,46 +22,63 @@ function NavButton() {
       
           setState({ ...state, [right]: open });
         };
-    const list = (anchor) =>(
-        <Box
-        sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+    const list = (anchor) => (
+      <Box
+        sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
         role="presentation"
         onClick={toggleDrawer(anchor, false)}
         onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <List>
-                <ListItemText style={{ textDecoration: "none" }} to="/" ml={2}>
-                    Vos oeuvres d'art favorites:
-                </ListItemText>
-                <List sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper' }}>
-                    {isSelected.map((item, index) => (
-                        <SelectedCardBody
-                            key={index}
-                            image={item?.webImage.url.replace("s0", "w310")}
-                            imagePopup={item?.webImage.url.replace("s0", "w3000")}
-                            title={item?.title}
-                            artist={item?.principalOrFirstMaker}
-                        />
-                    ))}
-                </List>
-            </List>
-        </Box>
-    );
-    return(
-        <div>
-            {['Mes Favoris'].map((anchor) => (
-                <React.Fragment key={anchor}>
-                <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-                <Drawer
-                    anchor={'right'}
-                    open={state[anchor]}
-                    onClose={toggleDrawer(anchor, false)}
-                >
-                    {list(anchor)}
-                </Drawer>
-                </React.Fragment>
+      >
+        <List>
+          <Typography
+            variant="titleDrawer"
+            gutterBottom
+            ml={2}
+            mt={4}
+            variant="button"
+          >
+            Vos oeuvres d'art favorites
+          </Typography>
+          <List
+            sx={{ width: "100%", maxWidth: 500, bgcolor: "background.paper" }}
+          >
+            {isSelected.map((item, index) => (
+              <SelectedCardBody
+                key={index}
+                image={item?.webImage.url.replace("s0", "w310")}
+                imagePopup={item?.webImage.url.replace("s0", "w3000")}
+                title={item?.title}
+                artist={item?.principalOrFirstMaker}
+              />
             ))}
-        </div>
+          </List>
+        </List>
+      </Box>
+    );
+    return (
+      <div>
+        {["Mes Favoris"].map((anchor) => (
+          <React.Fragment key={anchor}>
+            <Button
+              startIcon={<FavoriteIcon />}
+              variant="outlined"
+              onClick={toggleDrawer(anchor, true)}
+              sx={{
+                color: "secondary.main",
+              }}
+            >
+              {anchor}
+            </Button>
+            <Drawer
+              anchor={"right"}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+            >
+              {list(anchor)}
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </div>
     );
 }
 
